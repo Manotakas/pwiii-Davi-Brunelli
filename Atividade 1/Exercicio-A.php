bf9955b58d71570118f00daed8cc7b98eeca2396
Manotakas <takedi006@gmail.com>
2025-02-19 17:59:39 -0300

    atividade php

---
+++ Atividade 1/Exercicio-A.php
@@ -0,0 +1,18 @@
+<?php
+
+$n1 = 10;
+$n2 = 5;
+$n3 = 8;
+
+echo "$n1";
+echo "<br> +";
+echo"<br> $n2";
+echo "<br> +";
+echo"<br> $n3";
+
+$media = ($n1+$n2+$n3)/3;
+
+echo "<br> A media é ",$media
+
+
+?>
