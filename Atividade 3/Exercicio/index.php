79351b41d036be5416bb7368ce45768be89be810
Manotakas <takedi006@gmail.com>
2025-03-19 17:39:28 -0300

    começando o Index

---
+++ Atividade 3/Exercicio/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Comidas</title>
+</head>
+
+<body>
+    <h1>Gerenciamento de Comida</h1>
+
+    <div id="quadrado">
+
+        Adicionar comida:
+        <br><br>
+        <input type="text" placeholder="Comida">
+        <br>
+        <input type="text" placeholder="Origem">
+        <br>
+        <a href="https://huemint.com/back-gradient-2/"><input type="submit" placeholder="Enviar"></a>
+        
+    </div>
+</body>
+
+</html>
